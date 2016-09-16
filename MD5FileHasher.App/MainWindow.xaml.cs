50246560a8b3a99f4389637829e2aaea121e775e
jiri <jirka.turek@gmail.com>
2016-09-16 16:31:13 +0200

    Add project files.

---
+++ MD5FileHasher.App/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace MD5FileHasher.App
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_BtnClick(object sender, RoutedEventArgs e)
+        {
+            var filePath = GetFilePath();
+            HashLabel.Content = GetHashFromFilePath(filePath);
+        }
+
+        private string GetFilePath()
+        {
+            var openFileDialog = new OpenFileDialog();
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : "";
+        }
+
+        private string GetHashFromFilePath(string filePath)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                    using (var stream = File.OpenRead(filePath))
+                        return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "‌​").ToLower();
+            }
+            catch (Exception e)
+            {
+                return "you have to select a file...";
+            }
+        }
+    }
+}
